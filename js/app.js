59bd93ac5753067f7bb04106e2ee8d92ee2216d4
CalinCretu <calincretu1412@gmail.com>
2023-10-13 19:43:42 +0200

    ADD - htm-css-js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('prova')
+const gridElement = document.querySelector('.grid')
+const playBtnElement = document.getElementById('play-btn')
+playBtnElement.addEventListener('click', function () {
+  gridElement.innerHTML = ''
+  for (let i = 0; i < 100; i++) {
+    const n = i + 1
+    const htmlCell = '<div class="cell">' + n + '</div>'
+    gridElement.innerHTML = gridElement.innerHTML + htmlCell
+  }
+  const cellDomElements = document.querySelectorAll('.cell')
+  for (let i = 0; i < cellDomElements.length; i++) {
+    const currentCellElement = cellDomElements[i]
+    currentCellElement.addEventListener('click', function () {
+      console.log(i + 1)
+      currentCellElement.classList.add('bg-blue')
+    })
+  }
+})
+const cellDomElement = document.querySelector('.cell')
+function onCellClick() {
+  this.classList.add('bg-green')
+}
